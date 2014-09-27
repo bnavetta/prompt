619a780839ed0c03e65c350aa2371a84f244a80a
Ben Navetta <ben.navetta@gmail.com>
2014-09-27 17:17:42 -0400

    work on colors

--- src/prompt/hook.rs
+++ src/prompt/hook.rs
@@ -1,10 +1,39 @@
-use std::os;
+extern crate term;
 
-pub fn preexec(command: &str) -> String
+use std::os;
+use term::color;
+
+pub fn preexec(command: &str)
 {
-	return format!("\x1B]0;{}: {}\x07", os::getcwd().display(), command);
+	//print!("\x1B]0;{}: {}\x07", os::getcwd().display(), command);
 }
 
 pub fn preprompt() {
-	
+
+}
+
+pub fn prompt(exit_status: int) {
+	let mut t = term::stdout().unwrap();
+	if exit_status == 0
+	{
+		t.fg(color::BRIGHT_MAGENTA).unwrap();
+		// print!("%F{{magenta}}");
+	}
+	else
+	{
+		t.fg(color::BRIGHT_RED).unwrap();
+		// print!("%F{{red}}");
+	}
+	write!(t, "❯");
+	// t.reset().unwrap();
+	write!(t, "%f ");
+	// print!("❯%f ");
+}
+
+pub fn rprompt() {
+	let mut t = term::stdout().unwrap();
+	// t.fg(color::GREEN).unwrap();
+	write!(t, "%F{{green}}Hi");
+	// t.reset().unwrap();
+	write!(t, "%f");
 }
--- src/prompt/mod.rs
+++ src/prompt/mod.rs
@@ -1,4 +1,4 @@
-pub use self::hook::{preexec};
+pub use self::hook::{prompt,rprompt,preexec};
 pub use self::platform::{username,hostname};
 
 pub mod util;
