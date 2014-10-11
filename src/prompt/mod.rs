5b349b50c241805b76ba9f09b9043bfd1ae97a2f
Ben Navetta <ben.navetta@gmail.com>
2014-10-11 15:35:47 -0400

    finally remove platform.rs

--- src/prompt/mod.rs
+++ src/prompt/mod.rs
@@ -1,9 +1,7 @@
 pub use self::hook::{prompt,rprompt,preexec,precmd};
-pub use self::platform::{username,hostname};
 
 pub mod config;
 pub mod util;
 pub mod term;
 pub mod git;
 mod hook;
-mod platform;
