50b731ef02b3d8ab6154562dc15a751ce179bc92
Ben Navetta <ben.navetta@gmail.com>
2014-10-11 16:02:47 -0400

    change static to const

--- src/prompt/config.rs
+++ src/prompt/config.rs
@@ -2,12 +2,12 @@ use std::time::duration::Duration;
 use super::term;
 use super::term::Color;
 
-pub static COLOR_SUCCESS: Color = term::Green;
-pub static COLOR_FAILURE: Color = term::Red;
-pub static COLOR_EXEC_TIME: Color = term::Magenta;
-pub static COLOR_USER: Color = term::Cyan;
-pub static COLOR_HOST: Color = term::Yellow;
-pub static COLOR_PATH: Color = term::Blue;
+pub const COLOR_SUCCESS: Color = term::Green;
+pub const COLOR_FAILURE: Color = term::Red;
+pub const COLOR_EXEC_TIME: Color = term::Magenta;
+pub const COLOR_USER: Color = term::Cyan;
+pub const COLOR_HOST: Color = term::Yellow;
+pub const COLOR_PATH: Color = term::Blue;
 
 #[inline]
 pub fn max_exec_time() -> Duration {
