65c4afbc3312e4277a0fba3e2cfee5b194eb6483
Ben Navetta <ben.navetta@gmail.com>
2015-01-12 21:15:53 -0500

    start implementing personal prompt functions

--- src/bin/prompt_ben.rs
+++ src/bin/prompt_ben.rs
@@ -1,4 +1,7 @@
 extern crate prompt;
+extern crate term;
+
+use std::os;
 
 fn main() {
 	match prompt::mux::multiplexer() {
@@ -11,4 +14,38 @@ fn main() {
 		prompt::user::username().unwrap());
 
 	println!("Happily running on {} in {}", prompt::net::hostname(), prompt::cwd().display());
+
+	let mut t = term::stdout().unwrap();
+
+	rprompt(&mut *t);
+}
+
+fn precmd() {
+
+}
+
+fn main_prompt() {
+
+}
+
+fn rprompt(t: &mut term::Terminal<term::WriterWrapper>) {
+	let cwd = os::getcwd().unwrap();
+	match prompt::vcs::repo(&cwd) {
+		Some(repo) => {
+			let color = if repo.has_changes() {
+				term::color::RED
+			}
+			else if !repo.is_synchronized() {
+				term::color::YELLOW
+			}
+			else {
+				term::color::GREEN
+			};
+
+			t.fg(color).unwrap();
+			(writeln!(t, "{}{}", repo.current_branch(), repo.symbol())).unwrap();
+			t.reset().unwrap();
+		},
+		None => {},
+	}
 }
