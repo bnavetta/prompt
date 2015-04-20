b312753fff67150bd744d7273d6b186a83c45bfc
Ben Navetta <ben.navetta@gmail.com>
2015-04-19 23:38:26 -0400

    Clean up git module

--- src/vcs/git.rs
+++ src/vcs/git.rs
@@ -22,45 +22,15 @@ impl Vcs for Git {
 	}
 
 	fn is_synchronized(&self) -> VcsResult<bool> {
-		// let mut config_unstable = try!(self.repo.config());
-		// let config = try!(config_unstable.snapshot());
 		let branch_name = try!(self.current_branch());
 
-		// let remote_key = format!("branch.{}.remote", branch_name);
-		// let remote_name = try!(config.get_str(&remote_key));
-
-		// let remote_branch_name = format!("{}/{}", remote_name, branch_name);
-
 		let local_branch = try!(self.repo.find_branch(&branch_name, BranchType::Local));
-		// let remote_branch = try!(self.repo.find_branch(&remote_branch_name, BranchType::Remote));
-
 		let upstream_branch = try!(local_branch.upstream());
-
-		// println!("Upstream Branch: {:?}", upstream_branch.name());
-		// println!("Local Branch: {:?}", local_branch.name());
-		// println!("Remote Branch: {:?}", remote_branch.name());
 
 		let local_commit = try!(local_branch.get().resolve());
 		let remote_commit = try!(upstream_branch.get().resolve());
 
 		Ok(local_commit == remote_commit)
-
-		// let remote_branch_key = format!("branch.{}.merge", branch);
-		// let remote_branch = match config.get_str(&remote_branch_key) {
-		// 	Ok(branch) => branch[11..], // remove "refs/heads/master",
-		// 	Err(_)     => return false
-		// };
-		//
-		// let upstream = format!("refs/remotes/{}/{}", remote_name, remote_branch);
-		//
-		// let upstream_id = match self.repo.refname_to_id(upstream.as_slice()) {
-		// 	Ok(id) => id,
-		// 	Err(_) => return false,
-		// };
-		//
-		// let head_id = self.repo.refname_to_id("HEAD").unwrap(); // repo has to have a head
-		// let (ahead, behind) = self.repo.graph_ahead_behind(head_id, upstream_id).unwrap();
-		// ahead == 0 && behind == 0
 	}
 
 	fn has_changes(&self) -> VcsResult<bool> {
