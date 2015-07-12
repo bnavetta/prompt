e66409b7cb2cfcf0f537a4660e1e518c88f9b48e
Ben Navetta <ben.navetta@gmail.com>
2015-07-12 13:01:24 -0400

    update dependency versions and remove unneeded duration support

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ extern crate git2;
 
 use std::env;
 use std::path::Path;
-use std::time::duration;
 
 pub mod mux;
 pub mod net;
@@ -27,33 +26,4 @@ pub fn cwd() -> String {
 	};
 
 	format!("{}", cwd.display())
-}
-
-pub fn human_duration(duration: duration::Duration) -> String {
-	let mut result = String::new();
-
-	let days = duration.num_days();
-	let hours = duration.num_hours() % 24;
-	let minutes = duration.num_minutes() % 60;
-	let seconds = duration.num_seconds() % 60;
-
-	if days > 0 {
-		result.push_str(&days.to_string());
-		result.push_str("d ");
-	}
-
-	if hours > 0 {
-		result.push_str(&hours.to_string());
-		result.push_str("h ");
-	}
-
-	if minutes > 0 {
-		result.push_str(&minutes.to_string());
-		result.push_str("m ");
-	}
-
-	result.push_str(&seconds.to_string());
-	result.push_str("s");
-
-	result
 }
