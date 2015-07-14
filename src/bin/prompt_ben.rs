a321b9e6ceca3b7b9d1132ce8d2c450c719230bc
Ben Navetta <ben.navetta@gmail.com>
2015-07-14 17:12:03 -0400

    actual prompt using ZSH formatting instead of term crate

--- src/bin/prompt_ben.rs
+++ src/bin/prompt_ben.rs
@@ -1,54 +1,83 @@
 extern crate prompt;
-extern crate term;
 
 use std::env;
 use std::io;
 
+const ORANGE: &'static str = "166";
+
 fn main() {
-	match prompt::mux::multiplexer() {
-		Some(m) => println!("Multiplexer: {}", m),
-		None    => println!("No multiplexer"),
-	}
+    let mut args = env::args();
 
-	println!("{} {}!",
-		if prompt::user::is_root() { "Greetings, master" } else { "Hello," },
-		prompt::user::username().unwrap());
-
-	println!("Happily running on {} in {}", prompt::net::hostname().unwrap(), prompt::cwd());
-
-	let mut t = term::stdout().unwrap();
-
-	rprompt(&mut *t).unwrap();
+    args.next(); // First arg is program
+    let mode: &str = &args.next().expect("Must specify prompt mode");
+    match mode {
+        "prompt"  => main_prompt(args.next().expect("Missing exit status").parse::<i32>().unwrap()).unwrap(),
+        "rprompt" => rprompt(args.next().expect("Missing job count").parse::<u32>().unwrap()).unwrap(),
+        _         => panic!("Illegal usage: {}", mode),
+    }
 }
 
-fn precmd() {
+fn main_prompt(exit_status: i32) -> io::Result<()> {
+    let host_color = if prompt::net::is_ssh() { ORANGE } else { "yellow" };
 
+    let in_mux = prompt::mux::in_multiplexer();
+    if in_mux {
+        print!("%F{{cyan}}[%f");
+    }
+    else {
+        print!("[");
+    }
+
+    print!("%F{{magenta}}{}%f", prompt::user::username().unwrap());
+
+    print!("@");
+
+    print!("%F{{{}}}{}%f", host_color, prompt::net::hostname().unwrap());
+
+    if in_mux {
+        print!("%F{{cyan}}]%f");
+    }
+    else {
+        print!("]");
+    }
+
+    if prompt::user::is_root() {
+        print!("⚡");
+    }
+    else {
+        print!(" ");
+    }
+
+    print!("%F{{blue}}{}%f ", prompt::cwd());
+
+    let caret_color = if exit_status == 0 { "green" } else { "red" };
+    print!("%F{{{}}}❯ %f", caret_color);
+
+    Ok(())
 }
 
-fn main_prompt() {
+fn rprompt(job_count: u32) -> io::Result<()> {
+    if job_count != 0 {
+        print!("%F{{cyan}}⚙%f ");
+    }
 
-}
+	  let cwd = env::current_dir().unwrap();
+	  let vcs = prompt::vcs::repo(&cwd);
 
-fn rprompt(t: &mut term::StdoutTerminal) -> io::Result<()> {
-	let cwd = env::current_dir().unwrap();
-	let vcs = prompt::vcs::repo(&cwd);
+	  if vcs.is_ok() {
+		    let repo = vcs.unwrap();
+		    let color = if repo.has_changes().unwrap() {
+			      "red"
+		    }
+		    else if !repo.is_synchronized().unwrap() {
+			      "yellow"
+		    }
+		    else {
+			      "green"
+		    };
 
-	if vcs.is_ok() {
-		let repo = vcs.unwrap();
-		let color = if repo.has_changes().unwrap() {
-			term::color::RED
-		}
-		else if !repo.is_synchronized().unwrap() {
-			term::color::YELLOW
-		}
-		else {
-			term::color::GREEN
-		};
+        print!("%F{{{}}}{}{}%f", color, repo.symbol(), repo.current_branch().unwrap());
+	  }
 
-		try!(t.fg(color));
-		try!(writeln!(t, "{}{}", repo.symbol(), repo.current_branch().unwrap()));
-		try!(t.reset());
-	}
-
-	Ok(())
+	  Ok(())
 }
