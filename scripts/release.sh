fae692b7666aadd1ef7115e267cfd9a15e04215a
Ben Navetta <ben.navetta@gmail.com>
2019-04-24 16:24:47 -0400

    Fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,12 @@
 VERSION="$(cargo read-manifest | jq .version -r)"
 echo "Releasing $VERSION on GitHub"
 
+# Hub requires the actual filenames to be different 
+cp target/x86_64-apple-darwin/release/prompt target/x86_64-apple-darwin/release/prompt-darwin-x86_64
+cp target/x86_64-unknown-linux-gnu/release/prompt target/x86_64-unknown-linux-gnu/release/prompt-linux-x86_64
+
 hub release create \
-    -a target/x86_64-apple-darwin/release/prompt#prompt-darwin-x86_64
-    -a target/x86_64-unknown-linux-gnu/release/prompt#prompt-linux-x86_64
-    "$VERSION"
+    -a target/x86_64-apple-darwin/release/prompt-darwin-x86_64 \
+    -a target/x86_64-unknown-linux-gnu/release/prompt-linux-x86_64 \
+    -m "prompt_ben v$VERSION" -e \
+    "v$VERSION"
